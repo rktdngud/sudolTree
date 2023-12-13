45bfde6524e70cc4739a2cb49c19aeef17e8c0b2
rktdngud <dngud915@gmail.com>
2023-12-13 14:13:15 +0900

    server setting

---
+++ server/sudolTree/src/main/java/com/sudole/sudolTree/SudolTreeApplication.java
@@ -0,0 +1,13 @@
+package com.sudole.sudolTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudolTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudolTreeApplication.class, args);
+	}
+
+}
---
+++ server/sudolTree/src/test/java/com/sudole/sudolTree/SudolTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudole.sudolTree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudolTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
